e710e9b10d0cca01d0d3800dede5d67ba55cc2bf
Kristian Thy <thy@42.dk>
2020-07-22 00:20:24 +0200

    4.10: Blog list tests, step3

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -35,6 +35,23 @@ test('a blog has an id', async () => {
   ids.forEach(id => expect(id).toHaveLength(24))
 })
 
+test('a blog doc can be created', async () => {
+  let response = await api.get('/api/blogs')
+  const lengthBeforeInsert = response.body.length
+  await api
+    .post('/api/blogs')
+    .send({
+      title: '.NET Core Code Coverage as a Global Tool with coverlet',
+      author: 'Scott Hanselman',
+      url: 'https://www.hanselman.com/blog/NETCoreCodeCoverageAsAGlobalToolWithCoverlet.aspx',
+      likes: 1
+    })
+  response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(lengthBeforeInsert + 1)
+  const authors = response.body.map(r => r.author)
+  expect(authors).toContain('Scott Hanselman')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
